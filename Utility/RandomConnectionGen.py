e3e2d6e99bb5fd1daeca2089772f3ffdfb63c5fd
Yifei Wang <yifei666@umd.edu>
2022-04-20 23:39:23 -0400

    added random connection.json generator. Also modified some detials about the model

---
+++ Utility/RandomConnectionGen.py
@@ -0,0 +1,20 @@
+import numpy as np
+import json
+
+def RandomConnectionGenerator(nodes, querynum, bw, latencylimit):
+    connection = []
+    for i in range(querynum):
+        query = []
+        query.append(np.random.randint(1,(nodes+1)/2))
+        query.append(np.random.randint((nodes+1)/2, nodes + 1))
+        query.append(np.random.randint(1, bw+1))
+        query.append(np.random.randint(latencylimit, latencylimit+latencylimit/2))
+        connection.append(query)
+
+    with open('/Users/yifeiwang/Desktop/test214/pce/test/data/connection.json', 'w') as json_file:
+        data = connection
+        json.dump(data, json_file, indent=4)
+
+
+
+RandomConnectionGenerator(20,10,10,1000)
