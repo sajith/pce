1789271a0d6ffb4edfede4d49a5f2c4077325ed0
Yufeng Xin <yxin@renci.org>
2022-06-06 11:30:04 -0400

    added gaph connectivity check in the unittest

--- tests/LoadBalancing/test_MC_Solver.py
+++ tests/LoadBalancing/test_MC_Solver.py
@@ -1,5 +1,8 @@
 import unittest
 import json
+
+from networkx.algorithms import approximation as approx
+
 from LoadBalancing.MC_Solver import runMC_Solver
 from LoadBalancing.RandomTopologyGenerator import GetNetworkToplogy
 from LoadBalancing.RandomTopologyGenerator import GetConnection
@@ -16,11 +19,16 @@ class Test_MC_Solver(unittest.TestCase):
         self.connection = Connection
         self.topology = Topology
         self.solution = solution
+
+        con=approx.node_connectivity(self.topology)
+        print("Node connectivity:"+str(con))
+
         with open('./tests/data/connection.json', 'w') as json_file:
             json.dump(self.connection, json_file, indent=4)
 
     def test_Computation(self):
         lbnxgraphgenerator(25, 0.4, self.connection, self.topology)
+
         result = runMC_Solver()
 
         self.assertEqual(self.solution, result)
